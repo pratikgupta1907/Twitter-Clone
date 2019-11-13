a256254223563dfcab3bbbd01f9a20d4b914a700
pratik gupta <prati1907@gmail.com>
2019-11-13 18:00:38 +0530

    Initial Commit

---
+++ Twitter Clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Twitter Clone
+//
+//  Created by pratik gupta on 13/11/19.
+//  Copyright © 2019 pratik gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
